e96239dedccc9fa922445a4b0ba187c32935f823
Junde Li <jul1512@AD.PSU.EDU@E5-EE-W3031.ad.psu.edu>
2022-05-29 16:13:36 -0400

    modify loss

--- neuraldock.py
+++ neuraldock.py
@@ -15,18 +15,18 @@ from utils import *
 #############################
 # Hyperparameters
 #############################
-lr             = 1e-6
+lr             = 1e-5
 beta1          = 0.0
 beta2          = 0.9
 batch_size     = 16
-max_epoch      = 3000
+max_epoch      = 30000
 num_workers    = 2
 ligand_size    = 36
-save_step      = 200
+save_step      = 2000
 
-name = "neural"
-log_fname = f"{name}/logs"
-viz_dir = f"{name}/viz"
+name = "model/neuraldock"
+log_fname = f"{name}"
+viz_dir = f"{name}"
 models_dir = f"{name}/saved_models"
 
 if not os.path.exists(log_fname):
@@ -36,7 +36,7 @@ if not os.path.exists(viz_dir):
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
-class recEncoder(nn.Module):
+class RecEncoder(nn.Module):
     """Protein receptor encoding network."""
     def __init__(self):
         super(recEncoder, self).__init__()
@@ -58,7 +58,7 @@ class recEncoder(nn.Module):
         
         return h
 
-class ligEncoder(nn.Module):
+class LigEncoder(nn.Module):
     """Ligand encoding network."""
     def __init__(self):
         super(ligEncoder, self).__init__()
@@ -86,8 +86,8 @@ class NeuralDock(nn.Module):
     """Network for predicting docking energy."""
     def __init__(self):
         super(NeuralDock, self).__init__()
-        self.rec_enc = recEncoder()
-        self.lig_enc = ligEncoder()
+        self.rec_enc = RecEncoder()
+        self.lig_enc = LigEncoder()
         self.block1 = nn.Sequential(
             nn.Dropout(p=0.2),
             nn.utils.spectral_norm(nn.Linear(2048, 1024)),
@@ -114,7 +114,7 @@ class NeuralDock(nn.Module):
         return bd, stat
 
 # Make the optimizer.
-model = torch.nn.DataParallel(NeuralDock())
+model = torch.nn.DataParallel(NeuralDock()).cuda()
 optimizer = torch.optim.Adam(model.parameters(), lr)
 
 # Make the dataloaders.
@@ -140,35 +140,36 @@ test_loader = torch.utils.data.DataLoader(list(zip(receptor, atoms, bonds, bd)),
                         num_workers=num_workers)
 
 def main():
-    # checkpoint = torch.load(f"{models_dir}/neuraldock-2000.pth")
-    # model.load_state_dict(checkpoint['model_state_dict'])
-    # epoch_start = checkpoint['epoch']
-    epoch_start = 0
+    epoch_start = 16000
+    checkpoint = torch.load(f"{models_dir}/neuraldock-{epoch_start}.pth")
+    model.load_state_dict(checkpoint['model_state_dict'])
 
     # train loop
     print('Start traning...')
     for epoch in tqdm(range(epoch_start, max_epoch),  desc='total progress'):
         model.train()
-        losses = []
+        losses_bd, losses_stats = [], []
         for batch, (recs, atoms, bonds, bd, stats) in enumerate(train_loader):
             curr_log = f"epoch {epoch}\t"
-            print()
 
             # Train the model.
             optimizer.zero_grad()
-            bd_pred, stats_logits = model(recs, atoms, bonds)
-            print(bd_pred.shape, stats_logits.shape)
-            loss = torch.nn.MSELoss(reduction='mean')(bd_pred, bd) + \
-                torch.nn.MSELoss(reduction='mean')(stats_logits, stats)
+            bd_pred, stats_logits = model(recs.cuda(), atoms.cuda(), bonds.cuda())
+            loss_bd = torch.nn.MSELoss(reduction='mean')(bd_pred, bd.cuda())
+            loss_stats = torch.nn.MSELoss(reduction='mean')(stats_logits, stats.cuda())
+            loss = loss_bd + loss_stats
             loss.backward()
-            losses.append(loss.item())
-            print(f"{epoch}:{batch}\t{loss.item():.4f}", end="\r")
+
+            nn.utils.clip_grad_norm_(model.parameters(), 0.5)
             optimizer.step()
 
-        curr_log += f"loss:{np.mean(losses):.4f}\t"
-        print_and_save(curr_log, "pdb-cla.txt")
+            losses_bd.append(torch.sqrt(loss_bd).item())
+            losses_stats.append(torch.sqrt(loss_stats).item())
 
-        if (epoch+1) % save_step == 0 or np.mean(losses) < 650:
+        curr_log += f"loss:{np.mean(losses_stats):.4f}\t bdloss:{np.mean(losses_bd):.4f}"
+        print_and_save(curr_log, f"{log_fname}/torch-pdb-cla.txt")
+
+        if (epoch+1) % save_step == 0:
             torch.save({
                         'epoch': epoch+1,
                         'model_state_dict': model.state_dict(),
